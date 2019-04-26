6c63c6fe708de5937d15f9a58e34174280a0995f
Eward <471875949@qq.com>
2019-04-26 11:13:30 +0800

    the first commit for github
    Signed-off-by: Eward <471875949@qq.com>

--- day01/preprocessdata.py
+++ day01/preprocessdata.py
@@ -24,6 +24,28 @@ X[:,0] = x_labelencoder.fit_transform(X[:,0])
 print(X)
 x_onehot = OneHotEncoder(categorical_features=[0])
 
+# oneHotEncode()类的使用 --> (n_values,  categorical_features,  dtype,  sparse,  handle_unknown)
+# 首先搞懂fit transform 以及 fit_transform 的用法比较:
+# fit是生成transform的信息,用于分析数据
+# transform是将fit后产生的数据形式适用于数据
+# 而fit_transform是两者的结合
+
+# n_values: 代表每个维度数据,需要用几维的one_hot编码实现,默认为'auto'自动分析
+# e.g.:enc = OneHotEncoder(n_values = [2, 3, 4])
+#      enc.fit([[0, 0, 3],
+#               [1, 1, 0]])
+#      ans = enc.transform([[0, 2, 3]]).toarray()
+#      输出 [[ 1.  0.  0.  0.  1.  0.  0.  0.  1.]]
+#     ps:  如果训练样本中有丢失的分类特征值，我们就必须显示地设置参数 n_values 了，这样防止编码出错
+
+# categorical_features:对哪一维的数据进行编码 把需要编码的维度索引放在列表中
+
+# dtype:输出类型格式,默认为'numpy.float64'
+
+# sparse: 如果设置 False 与 transform后的.toarray()的参数效果相同
+
+# handle_unknown:
+
 lb = LabelBinarizer()
 Y = lb.fit_transform(Y)
 
